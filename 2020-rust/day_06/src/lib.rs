e7da145f5ff546e160350c09d73f8fa5568966b4
Daniel Lawrence <danlaw@amazon.com>
2020-12-07 09:37:29 -0500

    Add day_06 part 2 solution

--- 2020-rust/day_06/src/lib.rs
+++ 2020-rust/day_06/src/lib.rs
@@ -1,12 +1,29 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
 use regex::Regex;
 
-pub fn count_questions_for_group(input: &str) -> usize{
+pub fn count_questions_for_group(input: &str) -> usize {
     let c: HashSet<char> = input.trim()
     .chars().filter(|c| c!= &'\n' && c!=&' ')
     .collect();
     println!("{:#?}", c);
     return c.len();
+}
+
+pub fn count_questions_for_group_all_yes(input: &str) -> i32 {
+    let member_count = input.lines().count();
+    let char_counts = input.chars().filter(|c| c!= &'\n' && c!=&' ')
+    .fold(HashMap::new(), |mut freqs, value| {
+        *freqs.entry(value).or_insert(0) += 1;
+        freqs
+    });
+    let mut count = 0;
+    for (_key, value) in char_counts {
+        if value == member_count {
+            count = count + 1;
+        }
+    }
+    return count;
 }
 
 pub fn split_lines_on_empty_line(input: &str) -> Vec<&str> {
@@ -34,5 +51,13 @@ mod tests {
         y
         r";
         assert_eq!(count_questions_for_group(input), 3);
+    }
+
+    #[test]
+    fn test_count_questions_for_group_all_yes(){
+        let input = "a
+        a
+        a";
+        assert_eq!(count_questions_for_group_all_yes(input), 1);
     }
 }
--- 2020-rust/day_06/src/main.rs
+++ 2020-rust/day_06/src/main.rs
@@ -1,6 +1,7 @@
 use ::day_06;
 
 fn main () {
+    // part 1
     let input = include_str!("input.txt");
     let mut count = 0;
     day_06::split_lines_on_empty_line(input)
@@ -11,5 +12,16 @@ fn main () {
         count = count + thiscount;
     });
     println!("{}", count);
+    // part 2
+    let mut count = 0;
+    day_06::split_lines_on_empty_line(input)
+    .iter()
+    .for_each(|e|{
+        let thiscount = day_06::count_questions_for_group_all_yes(e);
+        println!("{} {}", e, thiscount);
+        count = count + thiscount;
+    });
+    println!("{}", count);
+
 }
 
